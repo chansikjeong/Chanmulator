b37e031b647bce3c26a8f3269319b07cc98c005e
chansikjeong <cjyong1004@naver.com>
2024-11-28 21:17:56 +0900

    🎉 add 아이디 생성 유효성 검사 기능 추가

---
+++ validator/user.validator.js
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+const signupSchema = Joi.object({
+  username: Joi.string().alphanum().lowercase().min(4).max(12).required(),
+  password: Joi.string().min(6).max(20).required(),
+});
+
+const userValidator = {
+  signupValidation: async (req, res, next) => {
+    const validation = signupSchema.validate(req.body);
+
+    if (validation.error) {
+      return res.status(400).json({ message: "잘못된 입력입니다." });
+    }
+
+    next();
+  },
+};
+
+export default userValidator;
